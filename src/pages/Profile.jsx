625c94217f4711fc9acd7b1d7e1a79a1e3921d7d
Irismiranda <110883702+Irismiranda@users.noreply.github.com>
2023-11-27 15:26:06 -0300

    width

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 export default function Profile(){
     return(
-        <div className="wrapper default_padding">
+        <div className="wrapper default_padding" style={{ width: standardWrapperWidth }}>
             <h1> Profile goes here</h1>
         </div>
     )
