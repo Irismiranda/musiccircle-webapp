dbb8717f6cf4c179c674aaa9f5f49ebf99c9e517
Irismiranda <110883702+Irismiranda@users.noreply.github.com>
2023-11-27 15:29:06 -0300

    aaa

--- src/pages/Inbox.jsx
+++ src/pages/Inbox.jsx
@@ -1,6 +1,9 @@
 import React from "react"
+import useStore from "../../store"
 
 export default function Inbox(){
+    const { standardWrapperWidth } = useStore()
+    
     return(
         <div className="wrapper default_padding" style={{ width: standardWrapperWidth }}>
             <h1> Inbox goes here</h1>
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,9 @@
 import React from "react"
+import useStore from "../../store"
 
 export default function Profile(){
+    const { standardWrapperWidth } = useStore()
+    
     return(
         <div className="wrapper default_padding" style={{ width: standardWrapperWidth }}>
             <h1> Profile goes here</h1>
