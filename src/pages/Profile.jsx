fc2d5ac42046479bf2f9f5567d843667efca7eb0
Irismiranda <110883702+Irismiranda@users.noreply.github.com>
2023-11-28 13:43:29 -0300

    Update Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,8 +1,12 @@
 import React from "react"
+import { useLocation } from "react-router-dom"
 import useStore from "../store"
 
 export default function Profile(){
     const { standardWrapperWidth } = useStore()
+    const location = useLocation()
+    const params = location.search
+    console.log("Log - params are:", params)
     
     return(
         <div className="wrapper default_padding" style={{ width: standardWrapperWidth }}>
