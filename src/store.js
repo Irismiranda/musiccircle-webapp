679fce3b31284b1fbb765c74ae92c6aab7f45866
Irismiranda <110883702+Irismiranda@users.noreply.github.com>
2023-11-29 13:07:51 -0300

    Update store.js

--- src/store.js
+++ src/store.js
@@ -12,7 +12,7 @@ const useStore = create((set, get) => ({
   setCurrentUser: (user) => set({ currentUser: user }),
   setSpotifyApi: (api) => set({ spotifyApi: api }),
   setArtistUri: (artist) => set({ artistUri: artist }),
-  setStandardWrapperWidth: (width) => set({ standardWrapperWidth: `calc(100vw - ${width}px - 25px)` - (window.innerWidth - document.documentElement.clientWidth) }),
+  setStandardWrapperWidth: (width) => set({ standardWrapperWidth: `calc(100vw - ${width}px - 25px - ${window.innerWidth - document.documentElement.clientWidth}px)` }),
   setSocket: (socket) => set({ socket: socket }),
   
 }))
