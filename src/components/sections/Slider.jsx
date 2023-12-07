5ecf431e505ebeda68fd53eb8d34dcc7f280cfc6
Irismiranda <110883702+Irismiranda@users.noreply.github.com>
2023-12-07 15:50:41 -0300

    Update Slider.jsx

--- src/components/sections/Slider.jsx
+++ src/components/sections/Slider.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import useStore from "../../store"
 import { Link } from "react-router-dom"
 import { Axios } from "../../Axios-config"
