2ab5076e4b1b9164c40e7d64e0157d26e49967e3
Irismiranda <110883702+Irismiranda@users.noreply.github.com>
2023-12-11 21:27:59 -0300

    Update store.js

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 import { create } from 'zustand'
+import { devtools } from 'zustand/middleware'
 
-const useStore = create((set, get) => ({
+const useStore = create(devtools((set, get) => ({
   accessToken: null,
   loggedUser: {},
   spotifyApi: null,
@@ -43,6 +44,6 @@ const useStore = create((set, get) => ({
     },
   })),
   
-}))
+})))
 
 export default useStore
