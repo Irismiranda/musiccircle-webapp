0887862931af9810eb256c58e0694e1028a95a2d
Irismiranda <110883702+Irismiranda@users.noreply.github.com>
2023-12-18 04:37:11 -0300

    Update ToggleFollowBtn.jsx

--- src/utils/ToggleFollowBtn.jsx
+++ src/utils/ToggleFollowBtn.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from "react"
 import useStore from "../store"
 import { Axios } from "../Axios-config"
+import { useParams } from "react-router-dom"
 
 export default function ToggleFollowBtn(props){
-    const [ isFollowing, setIsFollowing ] = useState(false)
     const { userId, setUserProfileData } = props
+    const [ isFollowing, setIsFollowing ] = useState(false)
     const { setLoggedUser, loggedUser } = useStore()
-
+    
     async function getIsFollowing(id){
         const response = await Axios.get(`/api/${loggedUser.id}/is_following/${id}`)
         setIsFollowing(response.data)
@@ -16,7 +17,12 @@ export default function ToggleFollowBtn(props){
         const response = await Axios.post(`/api/${loggedUser.id}/toggle_follow/${id}`)
         setIsFollowing(response.data.isFollowing)
         setLoggedUser(response.data.updatedLoggedUser)
-        setUserProfileData(response.data.updatedCurrentUser)
+        if(userId === loggedUser.id){
+            setUserProfileData(response.data.updatedLoggedUser)
+        } else {
+            setUserProfileData(response.data.updatedCurrentUser)
+        }
+        
         const stringData = JSON.stringify(response.data.updatedLoggedUser)
         localStorage.setItem("loggedUser", stringData)
     }
