55fca6cfe5a74190ba393baf74ae29dce391fc28
Irismiranda <110883702+Irismiranda@users.noreply.github.com>
2023-12-19 00:03:09 -0300

    Update List.jsx

--- src/components/sections/List.jsx
+++ src/components/sections/List.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react"
+import { Link } from "react-router-dom"
 import { PlayBtn } from "../../utils"
 
 export default function List(props){
@@ -11,19 +12,23 @@ export default function List(props){
         <div className="list_wrapper">
             {list.map((item, index) => {
                     return (
-                        <div key={item.id} style={{ position: "relative" }}>
-                            <div 
-                            className="flex"                          
-                            onMouseEnter={() => setHoverItemId(item.id)}
-                            onMouseLeave={() => setHoverItemId(null)}>
-                                <img src={`${item.imageUrl}`} 
-                                className="cover_small" />
-                                <div>
-                                    {category === "tracks" && <h5>{item.artistName}</h5>}
-                                    <h3>{index + 1}. {item.name}</h3>
-                                    <h4>{category.slice(0, -1)}</h4>
+                        <div 
+                        key={item.id} 
+                        style={{ position: "relative" }}>
+                            <Link to={category !== "tracks" ? `/${category.slice(0, -1)}/${item.id}` : ""}>
+                                <div 
+                                className="flex"                          
+                                onMouseEnter={() => setHoverItemId(item.id)}
+                                onMouseLeave={() => setHoverItemId(null)}>
+                                    <img src={`${item.imageUrl}`} 
+                                    className="cover_small" />
+                                    <div>
+                                        {category === "tracks" && <h5>{item.artistName}</h5>}
+                                        <h3>{index + 1}. {item.name}</h3>
+                                        <h4>{category.slice(0, -1)}</h4>
+                                    </div>
                                 </div>
-                            </div>
+                            </Link>
                             <div onMouseEnter={() => setHoverItemId(item.id)}>
                                 <PlayBtn 
                                 uri={item.uri} 
