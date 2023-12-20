d7337f71aa58647d292fbe202f99029be0b40928
Irismiranda <110883702+Irismiranda@users.noreply.github.com>
2023-12-20 00:33:50 -0300

    Update SearchMenu.jsx

--- src/components/menus/SearchMenu.jsx
+++ src/components/menus/SearchMenu.jsx
@@ -56,8 +56,8 @@ export default function SearchMenu(){
                 const normalizedSearchTerm = normalizeText(searchTerm).replace(/\s/g,'').toLowerCase()
                 const response = await Axios.get(`/api/search/user/${normalizedSearchTerm}`)
 
-                console.log("response is:", response.userData)
-                setSearchResults(response.userData)
+                console.log("response is:", response.data)
+                setSearchResults(response.data)
 
             } else {
                 setSearchResults(null)
