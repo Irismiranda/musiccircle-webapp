33b0eda4519675b164a495329599f978fe8cc4d0
Irismiranda <110883702+Irismiranda@users.noreply.github.com>
2023-12-21 00:55:17 -0300

    Update SliderScrollBtns.jsx

--- src/utils/SliderScrollBtns.jsx
+++ src/utils/SliderScrollBtns.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 import { SvgRightBtn, SvgLeftBtn } from "../assets"
 
 export default function SliderScrollBtns(props){
