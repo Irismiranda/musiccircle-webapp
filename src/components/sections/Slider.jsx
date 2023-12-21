8825788908688500f99e073c6063c0465b57855f
Irismiranda <110883702+Irismiranda@users.noreply.github.com>
2023-12-21 00:58:31 -0300

    Update Slider.jsx

--- src/components/sections/Slider.jsx
+++ src/components/sections/Slider.jsx
@@ -21,56 +21,58 @@ export default function Slider(props){
     }
 
     return(
-        <div 
-        className="slider_grid"
-        ref={parentRef}
-        style={{ position: "relative" }} >
-
+        <>
             <SliderScrollBtns 
             parentRef={parentRef}
             list={list}/>
-            {list
-                .filter(item => item.isVisible === visibility)
-                .sort((a, b) => {
-                    if (a.releaseDate && b.releaseDate) {
-                        return b.releaseDate.localeCompare(a.releaseDate)
-                    }
-                    return 0
-                })
-                .slice(type === "top_list" ? 0 : undefined, type === "top_list" ? 10 : undefined)
-                .map((item) => {
-                    return (
-                        <div className="slider_item_wrapper" key={item.id}>
-                            <div className="slider_image_wrapper">
-                                <Link to={category === "tracks" ? "" : `/${item.type}/${item.id}`}>
-                                    <div 
-                                    onMouseEnter={() => setHoverItemId(item.id)}
-                                    onMouseLeave={() => setHoverItemId(null)}
-                                    style={{ backgroundImage: `url('${item.imageUrl}')`}} className="cover_medium cover_wrapper">
+
+            <div 
+            className="slider_grid"
+            ref={parentRef}
+            style={{ position: "relative" }} >
+                {list
+                    .filter(item => item.isVisible === visibility)
+                    .sort((a, b) => {
+                        if (a.releaseDate && b.releaseDate) {
+                            return b.releaseDate.localeCompare(a.releaseDate)
+                        }
+                        return 0
+                    })
+                    .slice(type === "top_list" ? 0 : undefined, type === "top_list" ? 10 : undefined)
+                    .map((item) => {
+                        return (
+                            <div className="slider_item_wrapper" key={item.id}>
+                                <div className="slider_image_wrapper">
+                                    <Link to={category === "tracks" ? "" : `/${item.type}/${item.id}`}>
+                                        <div 
+                                        onMouseEnter={() => setHoverItemId(item.id)}
+                                        onMouseLeave={() => setHoverItemId(null)}
+                                        style={{ backgroundImage: `url('${item.imageUrl}')`}} className="cover_medium cover_wrapper">
+                                        </div>
+                                    </Link>
+                                    {isLoggedUser && <button onClick={() => toggleItemVisibility(item.id, item.type)}>{visibility ? "Hide" : "Show"}</button>}
+                                    <div onMouseEnter={() => setHoverItemId(item.id)}>
+                                        <PlayBtn 
+                                        uri={item.uri} 
+                                        id={item.id}
+                                        category={"slider"} 
+                                        type={item.type} 
+                                        hoverItemId={hoverItemId}/>
                                     </div>
-                                </Link>
-                                {isLoggedUser && <button onClick={() => toggleItemVisibility(item.id, item.type)}>{visibility ? "Hide" : "Show"}</button>}
-                                <div onMouseEnter={() => setHoverItemId(item.id)}>
-                                    <PlayBtn 
-                                    uri={item.uri} 
-                                    id={item.id}
-                                    category={"slider"} 
-                                    type={item.type} 
-                                    hoverItemId={hoverItemId}/>
                                 </div>
+                                <Link to={category === "tracks" ? "" : `/${item.type}/${item.id}`}>
+                                    <h3>{item.name}</h3>
+                                    {(category === "tracks" || category === "albums") && <h5>{item.artistName}</h5>}
+                                    <h4>{(category === "albums" || category === "albums") && 
+                                    <span>{item.releaseDate.slice(0, -6)} • </span>}
+                                    {category.slice(0, -1)}</h4>
+                                </Link>
                             </div>
-                            <Link to={category === "tracks" ? "" : `/${item.type}/${item.id}`}>
-                                <h3>{item.name}</h3>
-                                {(category === "tracks" || category === "albums") && <h5>{item.artistName}</h5>}
-                                <h4>{(category === "albums" || category === "albums") && 
-                                <span>{item.releaseDate.slice(0, -6)} • </span>}
-                                {category.slice(0, -1)}</h4>
-                            </Link>
-                        </div>
-                        )
-                    })   
-                }     
-        </div>
+                            )
+                        })   
+                    }     
+            </div>
+        </>
     )
 }
 
