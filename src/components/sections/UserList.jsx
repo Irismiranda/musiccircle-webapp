8418d45d2e82f8c3601a8317afab5dbea61d27c5
Irismiranda <110883702+Irismiranda@users.noreply.github.com>
2023-12-21 22:32:24 -0300

    Update UserList.jsx

--- src/components/sections/UserList.jsx
+++ src/components/sections/UserList.jsx
@@ -12,42 +12,45 @@ export default function UserSearchSection(props){
     }
 
     return (
-        list && list?.length > 0 ? (
-            list.map((user, index) => (
-                <section 
-                className="list_items_wrapper" 
-                style={{ overflowY: list.length > 3 ? "scroll" : ""  }}>
-                    <div 
-                    className="user_list_grid" 
-                    key={user.id}>
-                        <Link to={`/account/${user.id}`}>
-                            <img 
-                            className="profile_medium" 
-                            src={user.imageUrl} 
-                            alt={`Profile of ${user.name}`} />
-                        </Link>
-                        <Link to={`/account/${user.id}`}>
+        <section style={{ overflowY: list.length > 3 ? "scroll" : ""  }}>
+            {list && list.length > 0 ? (
+                list.map((user, index) => (
+                    <div className="list_items_wrapper" key={user.id}>
+                        <div className="user_list_grid">
+                            <Link to={`/account/${user.id}`}>
+                                <img
+                                    className="profile_medium"
+                                    src={user.imageUrl}
+                                    alt={`Profile of ${user.name}`}
+                                />
+                            </Link>
+                            <Link to={`/account/${user.id}`}>
                                 <h3>
-                                {user.name}
-                                <br/>
-                                <span className="user_handle">
-                                    @{user.userHandle}
-                                </span>
-                            </h3>
-                        </Link>
-                        {showBtn && <div
-                        className="flex" 
-                        style={{ justifyContent: "end" }}
-                        onClick={(e) => toggleTransparency(e)}>
-                            <ToggleFollowBtn 
-                            currentUserId={user.id} 
-                            setUserProfileData={setUserProfileData} />
-                        </div>}
+                                    {user.name}
+                                    <br />
+                                    <span className="user_handle">
+                                        @{user.userHandle}
+                                    </span>
+                                </h3>
+                            </Link>
+                            {showBtn && (
+                                <div
+                                    className="flex"
+                                    style={{ justifyContent: "end" }}
+                                    onClick={(e) => toggleTransparency(e)}
+                                >
+                                    <ToggleFollowBtn
+                                        currentUserId={user.id}
+                                        setUserProfileData={setUserProfileData}
+                                    />
+                                </div>
+                            )}
+                        </div>
                     </div>
-                </section>
-            ))
-          ) : (
-            <h3>Nothing to see here...</h3>
-          )
-      )
+                ))
+            ) : (
+                <h3>Nothing to see here...</h3>
+            )}
+        </section>
+    );
     }      
