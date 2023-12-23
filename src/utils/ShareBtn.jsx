14758a92d9486d5e47c4c4712ee19ac5ce1340a8
Irismiranda <110883702+Irismiranda@users.noreply.github.com>
2023-12-23 10:41:21 -0300

    Update ShareBtn.jsx

--- src/utils/ShareBtn.jsx
+++ src/utils/ShareBtn.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useRef } from "react"
+import { Link } from "react-router-dom"
 import { SvgShareBtn } from "../assets"
 import { ShareMenu } from "../components"
 import { useClickOutside } from "./utils"
