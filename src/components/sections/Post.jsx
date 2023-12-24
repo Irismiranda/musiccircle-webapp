d9f18a1479f7c7868ab79f8768149761419ec28c
Irismiranda <110883702+Irismiranda@users.noreply.github.com>
2023-12-24 16:52:38 -0300

    Update Post.jsx

--- src/components/sections/Post.jsx
+++ src/components/sections/Post.jsx
@@ -10,16 +10,16 @@ export default function Post(props){
 
     async function getitem(){
         if(data.type === "track"){
-            const item = await spotifyApi.getTrack(data[`${data.type}_id`])
+            const item = await spotifyApi.getTrack(data.id)
             const formatedItem = formatListData(item, "track")
             setItem(formatedItem)
         } else if(data.type === "album"){
-            const item = await spotifyApi.getAlbum(data[`${data.type}_id`])
+            const item = await spotifyApi.getAlbum(data.id)
             setItem(item)
             const formatedItem = formatListData(item, "album")
             setItem(formatedItem)
         } else if(data.type === "artist"){
-            const item = await spotifyApi.getArtist(data[`${data.type}_id`])
+            const item = await spotifyApi.getArtist(data.id)
             setItem(item)
             const formatedItem = formatListData(item, "artist")
             setItem(formatedItem)
@@ -33,24 +33,29 @@ export default function Post(props){
     }, [data])
 
     return (
-        <section 
-        key={data.id}
-        className="flex relative">
-            <div 
-            className="cover_medium"
-            style={{ backgroundImage: `url('${item?.imgUrl}')`}}
-            onMouseOver={() => setHoverItemId([`${data.type}_id`])}>
-            </div>
-            <PlayBtn 
-            uri={`spotify:${item.type}:${[`${data.type}_id`]}`} 
-            id={[`${data.type}_id`]}
-            category={"cover"} 
-            type={item.type} 
-            hoverItemId={hoverItemId}/>
-            <div>
-                <h3>{item.name}</h3>
-                <p>{data.comment}</p>
-            </div>
-        </section>
+        item && (
+            <section 
+                key={data.post_id}
+                className="flex relative">
+                <div 
+                    className="cover_medium"
+                    style={{ backgroundImage: `url('${item.imgUrl}')`}}
+                    onMouseOver={() => setHoverItemId(item.id)}
+                >
+                </div>
+                <PlayBtn 
+                    uri={`spotify:${item.type}:${item.id}`} 
+                    id={item.id}
+                    category={"cover"} 
+                    type={item.type} 
+                    hoverItemId={hoverItemId}
+                />
+                <div>
+                    <h3>{item.name}</h3>
+                    <p>{data.comment}</p>
+                </div>
+            </section>
+        )
     )
+    
 }
