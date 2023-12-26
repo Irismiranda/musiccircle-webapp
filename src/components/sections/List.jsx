2218cca82700db790da7031cb9e29bfd2fecac00
Irismiranda <110883702+Irismiranda@users.noreply.github.com>
2023-12-26 04:50:11 -0300

    Update List.jsx

--- src/components/sections/List.jsx
+++ src/components/sections/List.jsx
@@ -1,50 +1,47 @@
-import React, { useEffect, useState } from "react"
-import { Link } from "react-router-dom"
-import { PlayBtn } from "../../utils"
+    import React, { useEffect, useState } from "react"
+    import { Link } from "react-router-dom"
+    import { PlayBtn } from "../../utils"
 
-export default function List(props){
-    const { list, category, showIndex } = props
-    const [ hoverItemId, setHoverItemId ] = useState(null)
+    export default function List(props){
+        const { list, category, showIndex } = props
+        const [ hoverItemId, setHoverItemId ] = useState(null)
 
-    useEffect(() => {
-        console.log("hover item id is:", hoverItemId)
-    }, [hoverItemId])
-
-    return(
-        <div className="list_wrapper relative">
-            {list.map((item, index) => {
-                    return (
-                        <div 
-                        key={item.id}>
-                            <Link 
-                            to={item.type !== "track" ? `/${item.type}/${item.id}` : ""}
-                            onClick={(item.type === "track") && ((e) => e.preventDefault())}>
-                                <div 
-                                className="flex"                          
-                                onMouseEnter={() => setHoverItemId(item.id)}
-                                onMouseLeave={() => setHoverItemId(null)}>
-                                    <img src={`${item.imgUrl}`} 
-                                    className="cover_small" />
-                                    <div>
-                                        {category === "tracks" && <h5>{item.artistName}</h5>}
-                                        <h3>{showIndex && <span>{index + 1}.</span>} {item.name}</h3>
-                                        <h4>{item.type}</h4>
+        return(
+            <div className="list_wrapper">
+                {list.map((item, index) => {
+                        return (
+                            <div 
+                            className="relative"
+                            key={item.id}>
+                                <Link 
+                                to={item.type !== "track" ? `/${item.type}/${item.id}` : ""}
+                                onClick={(item.type === "track") && ((e) => e.preventDefault())}>
+                                    <div 
+                                    className="flex"                          
+                                    onMouseEnter={() => setHoverItemId(item.id)}
+                                    onMouseLeave={() => setHoverItemId(null)}>
+                                        <img src={`${item.imgUrl}`} 
+                                        className="cover_small" />
+                                        <div>
+                                            {category === "tracks" && <h5>{item.artistName}</h5>}
+                                            <h3>{showIndex && <span>{index + 1}.</span>} {item.name}</h3>
+                                            <h4>{item.type}</h4>
+                                        </div>
                                     </div>
+                                </Link>
+                                <div onMouseEnter={() => setHoverItemId(item.id)}>
+                                    <PlayBtn 
+                                    uri={item.uri} 
+                                    id={item.id}
+                                    category={"list"} 
+                                    type={item.type} 
+                                    hoverItemId={hoverItemId}/>
                                 </div>
-                            </Link>
-                            <div onMouseEnter={() => setHoverItemId(item.id)}>
-                                <PlayBtn 
-                                uri={item.uri} 
-                                id={item.id}
-                                category={"list"} 
-                                type={item.type} 
-                                hoverItemId={hoverItemId}/>
                             </div>
-                        </div>
-                        )
-                    })   
-                }     
-        </div>
-    )
-}
+                            )
+                        })   
+                    }     
+            </div>
+        )
+    }
 
