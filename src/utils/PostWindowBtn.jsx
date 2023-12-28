f16ec423536222fd9edb91ce25d947af4d305b3d
Irismiranda <110883702+Irismiranda@users.noreply.github.com>
2023-12-28 01:01:32 -0300

    Update PostWindowBtn.jsx

--- src/utils/PostWindowBtn.jsx
+++ src/utils/PostWindowBtn.jsx
@@ -117,7 +117,7 @@ export default function PostWindowBtn(props){
                             className={showFullDescription ? "" : "hide_description"}
                             ref={descriptionRef}>
                                 {data?.comment}
-                                { (descriptionRef?.current?.scrollHeight > 325) &&
+                                { (descriptionRef?.current?.scrollHeight > 200) &&
                                 <span 
                                 className="show_more"
                                 onClick={() => setShowFullDescription(!showFullDescription)}>
