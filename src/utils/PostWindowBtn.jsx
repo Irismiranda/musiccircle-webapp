c7d123442a77f814a66f2da7a32ca69a5c4490f2
Irismiranda <110883702+Irismiranda@users.noreply.github.com>
2023-12-27 23:30:50 -0300

    Update PostWindowBtn.jsx

--- src/utils/PostWindowBtn.jsx
+++ src/utils/PostWindowBtn.jsx
@@ -25,6 +25,7 @@ export default function PostWindowBtn(props){
         const methodName = `get${type.charAt(0).toUpperCase() + type.slice(1)}`
         const data = await spotifyApi[methodName](id)
         const formatedItem = formatListData([data], `${type}s`)
+        console.log("formated data is", formatedItem)
         setPostData(formatedItem[0])
     }
 
