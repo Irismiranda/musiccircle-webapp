7e4d6b6c2fe5f8caf1c7c683796d2c516138657d
Irismiranda <110883702+Irismiranda@users.noreply.github.com>
2024-01-03 16:41:06 -0300

    Update CommentBtn.jsx

--- src/utils/CommentBtn.jsx
+++ src/utils/CommentBtn.jsx
@@ -67,7 +67,7 @@ export default function CommentBtn(props){
 
     async function likePost(){
         await Axios.post(`/api/${user?.id}/${artistId}/toggle_like_post/${data.post_id}`, {
-            logged_user_id: loggedUser
+            logged_user_id: loggedUser.id
         })
     }
 
