417c17785a4d2d48700b104ed8d8cac87ea6f8b1
Irismiranda <110883702+Irismiranda@users.noreply.github.com>
2024-01-02 21:46:33 -0300

    Update Comments.jsx

--- src/components/sections/Comments.jsx
+++ src/components/sections/Comments.jsx
@@ -74,7 +74,7 @@ export default function Comments(props) {
                 console.log("updated comments are", updatedComments)
                 setComments(updatedComments)
             } else if (call === "loadNewComment" 
-            && !comments.some((comment) => comment.post_id === data[0].post_id)){
+            && comments.length > 0 && !comments.some((comment) => comment.post_id === data[0].post_id)){
                 setComments(prevComments => [...prevComments, updatedComments[0]])
             } else{
                 setComments((prevComments) =>
