5f4ce546f4d9305b7ddc5b149dfa7a3d21faa275
Irismiranda <110883702+Irismiranda@users.noreply.github.com>
2024-01-04 15:32:32 -0300

    Update Comments.jsx

--- src/components/sections/Comments.jsx
+++ src/components/sections/Comments.jsx
@@ -190,7 +190,7 @@ export default function Comments(props) {
         ref={commentsRef}
         className="flex flex_column comments_inner_wrapper"
         style={{ height: 
-        `calc(100% - ${descriptionMenuRef?.current?.clientHeight - sendPostRef?.current?.clientHeight}px)` }}>
+        `calc(100% - ${descriptionMenuRef?.current?.clientHeight}px - ${sendPostRef?.current?.clientHeight}px)` }}>
             {(comments?.length > 0) &&
             comments
             .sort((a, b) => (convertTimestampToDate(b.timestamp) > convertTimestampToDate(a.timestamp) ? -1 : 1))
@@ -296,7 +296,7 @@ export default function Comments(props) {
         <div 
         className="loading_comments"
         style={{ height: 
-        `calc(100% - ${descriptionMenuRef?.current?.clientHeight + sendPostRef?.current?.clientHeight}px)` }}>
+        `calc(100% - ${descriptionMenuRef?.current?.clientHeight}px)` }}>
         </div>
         )
     )
