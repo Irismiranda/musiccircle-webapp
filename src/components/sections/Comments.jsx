a29b5b4bcb3f5521de671bdc5b73247398d2caaf
Irismiranda <110883702+Irismiranda@users.noreply.github.com>
2024-01-07 16:23:49 -0300

    Update Comments.jsx

--- src/components/sections/Comments.jsx
+++ src/components/sections/Comments.jsx
@@ -37,7 +37,7 @@ export default function Comments(props) {
                 }
             })
 
-            const scrollOnLoad = (data[0].user_id === loggedUser.id)
+            const scrollOnLoad = (data[0]?.user_id === loggedUser.id)
 
             console.log("scroll on load?", scrollOnLoad)
 
