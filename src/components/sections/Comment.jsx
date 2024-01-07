2cacf03b785440fc489373982e57b47f7eed7d11
Irismiranda <110883702+Irismiranda@users.noreply.github.com>
2024-01-07 19:57:42 -0300

    Update Comment.jsx

--- src/components/sections/Comment.jsx
+++ src/components/sections/Comment.jsx
@@ -57,7 +57,7 @@ const Comment = React.memo((props) => {
             const prevRepliesIds = replies?.map(reply => reply.reply_id)
             const newReplyIds = data.replies?.map(reply => reply.reply_id)
             
-            const newReplyId = newReplyIds.find(reply => !(prevRepliesIds?.includes(reply.reply_id)))
+            const newReplyId = newReplyIds.find(id => !prevRepliesIds?.includes(id))
             const newReply = data.replies.find(reply => reply.reply_id === newReplyId)
         
             console.log("new reply is", newReply)
@@ -78,8 +78,7 @@ const Comment = React.memo((props) => {
                         prevReply.reply_id === updatedReply.reply_id ?
                             updatedReply : prevReply
                     ))
-                )
-                
+                ) 
             }
         }
         
